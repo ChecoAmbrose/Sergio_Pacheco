5a77d410f3303727fce4a315361d89f49e911064
Checo <elite_shergio@hotmail.es>
2021-03-06 16:49:42 -0600

    Tarea 5 Finalizada. Archivos: Tarea5.py | Tarea5.ui | EstudiantesMongo.py

--- GUI/Tarea5.py
+++ GUI/Tarea5.py
@@ -2,37 +2,53 @@ from EstudiantesMongo import Estudiantes
 from EstudiantesMongo import agregar
 from EstudiantesMongo import actualizar
 from EstudiantesMongo import eliminar
+import re
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QLineEdit
 
 class Tarea5(QMainWindow):
 
     def __init__(self):
         super().__init__()
         uic.loadUi("Tarea5.ui", self)
+        self.botonAgregar.setEnabled(False)
+        self.botonEliminar.setEnabled(False)
+        self.botonActualizar.setEnabled(False)
         self.botonAgregar.clicked.connect(self.agregarui)
         self.botonVer.clicked.connect(self.leerui)
         self.botonEliminar.clicked.connect(self.borrarui)
         self.botonActualizar.clicked.connect(self.actualizarui)
+        self.botonMostrar.clicked.connect(self.MostrarOcultar)
+        self.botonAyuda.clicked.connect(self.aiuda)
 
+        self.contrasena.setEchoMode(QLineEdit.Password)
         self.nombre.textEdited.connect(self.textoEditado)
         self.correo.textEdited.connect(self.textoEditado)
         self.contrasena.textEdited.connect(self.textoEditado)
         self.materias.textEdited.connect(self.textoEditado)
 
     def agregarui(self):
+        patroncorreo = "\S+[@]\S+[.]+[a-z]{2,3}$"
         self.statusBar().showMessage('Agregando a la base de datos...')
         name = self.nombre.text()
         mail = self.correo.text()
         passw = self.contrasena.text()
         mat = self.materias.text()
-        actual = Estudiantes(nombre = name, correo= mail, contrasena= passw, materias= mat )
-        agregar(actual)
-        self.statusBar().showMessage('Estudiante agregado!')
+        match = re.match(patroncorreo, mail)
+        if match:
+            actual = Estudiantes(nombre=name, correo=mail, contrasena=passw, materias=mat)
+            agregar(actual)
+            self.salida.append(f'Se agregó a {name} a la base de datos.')
+            self.statusBar().showMessage('Estudiante agregado!')
+        else:
+            #self.salida.append('Correo invalido, no se pudo agregar usuario')
+            self.salida.insertHtml('<p style="color: red"><br>Correo invalido, no se pudo agregar usuario </p>')
+            self.salida.insertHtml('<p style="color: black"><br></p>')
         pass
 
     def leerui(self):
+        self.salida.setText('')
         for estudiantes in Estudiantes.objects:
             #print(f'Nombre: {estudiantes.nombre}\tCorreo: {estudiantes.correo}\tContraseña: {estudiantes.contrasena}\tMaterias: {estudiantes.materias}')
             mensaje = f'Nombre: {estudiantes.nombre} Correo: {estudiantes.correo} Contraseña: {estudiantes.contrasena} Materias: {estudiantes.materias}'
@@ -44,17 +60,20 @@ class Tarea5(QMainWindow):
     def borrarui(self):
         funar = self.nombre.text()
         eliminar(funar)
+        self.salida.append(f'{funar} eliminad@ de la base de datos')
         pass
 
     def actualizarui(self):
         en = self.nombre.text()
         reemplazar = self.materias.text()
         actualizar(en, reemplazar)
+        self.salida.append(f'Se actualizó la materia en curso de {en}')
         pass
 
     def textoEditado(self):
         self.statusBar().showMessage('Escribiendo...')
 
+        #Agregar
         if len(self.nombre.text()) >= 3 \
             and len(self.correo.text()) >= 5 \
                 and len(self.contrasena.text()) >= 8 \
@@ -63,6 +82,7 @@ class Tarea5(QMainWindow):
         else:
             self.botonAgregar.setEnabled(False)
 
+        #Borrar
         if len(self.nombre.text()) >= 3 \
             and len(self.correo.text()) == 0 \
                 and len(self.contrasena.text()) == 0 \
@@ -71,6 +91,7 @@ class Tarea5(QMainWindow):
         else:
             self.botonEliminar.setEnabled(False)
 
+        #Actualizar
         if len(self.nombre.text()) >= 3 \
             and len(self.correo.text()) == 0 \
                 and len(self.contrasena.text()) == 0 \
@@ -79,20 +100,25 @@ class Tarea5(QMainWindow):
         else:
             self.botonActualizar.setEnabled(False)
 
+    #Mostrar Ocultar Contraseña
+    def MostrarOcultar(self):
+
+        if self.contrasena.echoMode() == QLineEdit.Password:
+            self.contrasena.setEchoMode(QLineEdit.Normal)
+            self.botonMostrar.setText('Ocultar')
+        else:
+            self.contrasena.setEchoMode(QLineEdit.Password)
+            self.botonMostrar.setText('Mostrar')
+
+    def aiuda(self):
+        self.salida.setText('')
+        self.salida.insertHtml('<p style="color: blue">Para agregar a un alumno a la base de datos, debes llenar todos los campos, el nombre debe ser mayor a 3 caracteres, el correo electrónico debe ser válido, la contraseña debe ser mayor a 8 caracteres.<br><br>Para actualizar la materia de un alumno debes escribir su nombre y la materia nueva que esté cursando. El campo de correo y de contraseña deben estar vacios.<br><br>Para eliminar a un alumno de la base de datos debes escribir su nombre, los demás campos deben estar vacíos.</p>')
+        self.salida.insertHtml('<p style="color: black"><br></p>')
+
 
 if __name__ == "__main__":
     app = QApplication([])
     widget = Tarea5()
     widget.show()
     sys.exit(app.exec_())
-
-
-#leer()
-
-#actualizar("Sergio", "Circuitos")
-#leer()
-
-#funar = "Emmanuel"
-#eliminar(funar)
-#eer()
 
